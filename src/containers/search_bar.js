1fada7d30970c55930d13cfb72351b851c1f673c
Em01 <emma_williams1@hotmail.co.uk>
2017-01-26 14:10:55 +0000

    further work on search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,9 +13,13 @@ export default class SearchBar extends Component {
     this.setState({term: event.target.value })
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favourite cities"
           className="form-control"
