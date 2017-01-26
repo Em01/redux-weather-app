fdb05928e5d7371672fc20f750e28bb7823d2d3d
Em01 <emma_williams1@hotmail.co.uk>
2017-01-26 15:50:14 +0000

    use axios for ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios'
+
 const API_KEY = '925cdbd0936279d4dce97283c7f21fef'
+const URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
