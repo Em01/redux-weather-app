aee9e575a71b1557fa116a4420a5f8c3427f02af
Em01 <emma_williams1@hotmail.co.uk>
2017-01-28 17:50:15 +0000

    add google-maps

--- src/components/chart.js
+++ src/components/chart.js
@@ -9,7 +9,7 @@ function average(data) {
 export default (props) => {
   return (
     <div>
-      <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
+      <Sparklines data={props.data}>
         <SparklinesLine color={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
 
  class WeatherList extends Component {
 
@@ -10,10 +11,11 @@ import Chart from '../components/chart'
      const toCelsius = temps.map(temp => temp - 273)
      const pressure = cityData.list.map(weather => weather.main.pressure)
      const humidity = cityData.list.map(weather => weather.main.humidity)
+     const { lat, lon } = cityData.city.coord;
      //console.log(temps, 'temps')
      return (
        <tr key={name}>
-          <td>{name}</td>
+          <td><GoogleMap lat={lat} lon={lon} /></td>
           <td><Chart data={toCelsius} color="orange" units="celsius"/></td>
           <td><Chart data={pressure} color="green" units="hPa"/></td>
           <td><Chart data={humidity} color="black" units="%"/></td>
